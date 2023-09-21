4fc1888e5419900a8c2cbdcea4aaf3a02668bad7
justinliu <nhliupm@aliyun.com>
2023-09-21 13:13:26 +0800

    add ZipUtils

--- src/main/java/com/example/demo/util/ZipUtils.java
+++ src/main/java/com/example/demo/util/ZipUtils.java
@@ -1,6 +1,7 @@
 package com.example.demo.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,6 +9,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -15,57 +18,87 @@ import java.util.zip.ZipOutputStream;
 public class ZipUtils {
     private ZipUtils() {}
 
-    public static void zipFolder(String sourceDir, String zipFileName) throws IOException {
+    public static void zipFolder(String baseDir, List<String> subDirs, String zipFileName) throws IOException {
         Files.deleteIfExists(Path.of(zipFileName));
         File zipFile = new File(zipFileName);
         try (FileOutputStream fos = new FileOutputStream(zipFile);
              ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-            File fileSource = new File(sourceDir);
-            addDirectory(zos, sourceDir, fileSource);
-            log.info("Zip file has been created!");
+            File fileSource = new File(baseDir);
+            addDirectory(zos, baseDir, subDirs, fileSource);
+            log.info("Zip file [{}] has been created!", zipFileName);
         }
     }
 
-    private static String getRelativePath(String sourceDir, File file) {
+    private static String getRelativePath(String baseDir, File file) {
         // Trim off the start of source dir path...
-        String path = file.getPath().substring(sourceDir.length());
+        String path = file.getPath().substring(baseDir.length());
         if (path.startsWith(File.separator)) {
             path = path.substring(1);
         }
         return path.trim();
     }
 
-    private static void addDirectory(ZipOutputStream zos, String sourceDir, File fileSource) throws IOException {
+    private static void addDirectory(ZipOutputStream zos, String baseDir, List<String> subDirs, File fileSource) throws IOException {
         if (fileSource.isDirectory()) {
             File[] files = fileSource.listFiles();
             if (files != null) {
+                Arrays.sort(files);
                 for (File file : files) {
                     if (file.isDirectory()) {
-                        addDirectory(zos, sourceDir, file);
+                        String relativePath = getRelativePath(baseDir, file);
+                        if (CollectionUtils.isEmpty(subDirs) || isSubDirMatched(relativePath, subDirs, true)) {
+                            log.debug("Add directory {}", relativePath);
+                            ZipEntry ze = new ZipEntry(relativePath + File.separator);
+                            ze.setTime(file.lastModified());
+                            zos.putNextEntry(ze);
+                            zos.closeEntry();
+                            addDirectory(zos, baseDir, subDirs, file);
+                        }
                     } else {
-                        addFile(zos, sourceDir, file);
+                        addFile(zos, baseDir, subDirs, file);
                     }
                 }
             }
         }
     }
 
-    private static void addFile(ZipOutputStream zos, String sourceDir, File file) throws IOException {
-        String filePath = getRelativePath(sourceDir, file);
-        log.info("Adding file " + filePath);
-        ZipEntry ze = new ZipEntry(filePath);
-        ze.setTime(file.lastModified());
-        zos.putNextEntry(ze);
+    private static void addFile(ZipOutputStream zos, String baseDir, List<String> subDirs, File file) throws IOException {
+        String relativePath = getRelativePath(baseDir, file);
+        if (CollectionUtils.isEmpty(subDirs) || isSubDirMatched(relativePath, subDirs, false)) {
+            log.debug("Adding file {}", relativePath);
+            ZipEntry ze = new ZipEntry(relativePath);
+            ze.setTime(file.lastModified());
+            zos.putNextEntry(ze);
 
-        try (FileInputStream fis = new FileInputStream(file)) {
-            int length;
-            byte[] buffer = new byte[1024];
-            while ((length = fis.read(buffer)) > 0) {
-                zos.write(buffer, 0, length);
+            try (FileInputStream fis = new FileInputStream(file)) {
+                int length;
+                byte[] buffer = new byte[1024];
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+            }
+
+            zos.closeEntry();
+        }
+    }
+
+    private static boolean isSubDirMatched(String filePath, List<String> subDirs, boolean isDirectory) {
+        boolean isMatched = false;
+        for (String subDir : subDirs) {
+            if (isDirectory) {
+                if (filePath.contains(subDir)) {
+                    isMatched = true;
+                }
+            } else {
+                if (filePath.contains(subDir + File.separator)) {
+                    isMatched = true;
+                }
+            }
+            if (isMatched) {
+                break;
             }
         }
-
-        zos.closeEntry();
+        return isMatched;
     }
 }
--- src/test/java/com/example/demo/ZipTest.java
+++ src/test/java/com/example/demo/ZipTest.java
@@ -2,15 +2,18 @@ package com.example.demo;
 
 
 import com.example.demo.util.ZipUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+@Slf4j
 class ZipTest {
 
     @Test
     void zipFolder() throws IOException {
-        ZipUtils.zipFolder("/home/justin/Documents/Picture", "/home/justin/Sandbox/test.zip");
+        ZipUtils.zipFolder("/home/justin/Documents/Picture", null,"/home/justin/Sandbox/test.zip");
     }
+
 
 }
